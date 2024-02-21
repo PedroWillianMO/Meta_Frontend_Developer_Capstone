40e263c6da120cbef1945c53ecb78c41dfe64b63
Pedro Willian Marques de Oliveira <pedrowiwgm@gmail.com>
2024-02-21 18:38:22 -0300

    first commit

---
+++ src/recipes.js
@@ -0,0 +1,31 @@
+const recipes = [
+    { 
+      id: 1,
+      title: "Especial salad",
+      price: 7.77,
+      image: "https://i.ibb.co/68c9bLv/greek-salad.jpg",
+      description:
+        "Indulge in our legendary Greek salad, boasting a medley of crisp lettuce, colorful peppers, plump olives, and our exclusive Chicago-style feta cheese. Enhanced with tantalizing garlic and rosemary-infused croutons, it's a culinary masterpiece that'll tantalize your taste buds.",
+    },
+    {
+      id: 2,
+      title: "Bruchetta",
+      price: 7.77,
+      image: "https://i.ibb.co/317GqZv/Bruchetta.png",
+      description:
+        "Crafted with care, our Bruschetta features grilled bread generously coated with garlic, delicately seasoned with salt, and drizzled with the finest olive oil for a delectable culinary experience.",
+    },
+    {
+      id: 3,
+      title: "Lemon ",
+      price: 7.77,
+      image: "https://i.ibb.co/sm6MfcH/desert.jpg",
+      description:
+        "Derived from a time-honored recipe, every ingredient has been carefully sourced to preserve its authenticity, resulting in a culinary experience that's beyond compare.",
+    },
+  
+  ];
+  
+  export default recipes;
+  
+  
